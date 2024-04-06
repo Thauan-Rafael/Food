d5798cd1ca4ad794f02c5b86468aad431386f17d
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-04-06 19:37:52 -0300

    Moved Content code to Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,77 @@
+import pizza from '../media/pizzaImg.png'
+import hamburger from '../media/hamburgerImg.png'
+import sushi from '../media/sushiImg.png'
+import cake from '../media/cakeImg.png'
+import iceCream from '../media/iceCreamImg.png'
+import drink from '../media/drinkImg.png'
+function Home(){
+    return(
+      <>
+        <section id="categories" className="container">
+            <div className="row">
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Pizza</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={pizza}></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Hamburger</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={hamburger}></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Japanese Food</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={sushi}></img>
+                        </div>
+                    </div>
+                </div>  
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Cakes</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={cake}></img>
+                        </div>
+                    </div>
+                </div>  
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Ice Cream</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={iceCream}></img>
+                        </div>
+                    </div>
+                </div>  
+                <div className="categories-column col-sm-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Drinks</h3>
+                        </div>
+                        <div className="card-body d-grid">
+                            <img className="card-image" src={drink}></img>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </section>
+      </>
+    )
+}
+export default Home;
